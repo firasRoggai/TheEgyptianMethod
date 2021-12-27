6bee2f28a2c304d517d3bd0db64b2187b072a1f0
firasRoggai <96572225+firasRoggai@users.noreply.github.com>
2021-12-27 20:29:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,29 +4,36 @@
 int main()
 {
 int a,b,counter1,counter2;
-counter1 = 0;
-counter2 = 0;
 printf("Enter A and B :\n");
 scanf("%d",&a);
 scanf("%d",&b);
 if (ceil(log2(b) == floor(log2(b)))){ // to ckeck if B is a power of 2.
     while (b != 1){
+    printf("%d x 2 = %d \n",a,a*2);
     a = a * 2;
     b = b/2;
 }
+printf("The result is :%d",a);
 }
 else if(b == 0){
     printf("0"); // a * 0 = 0
 }
 else {
-    printf("b is not power of 2\n");
+    printf("b is not power of 2");
     while (ceil(log2(b) != floor(log2(b)))){
-        b = b - 1;
+        b -= 1;
         counter1++;
     }
-    printf("B is %d \n",b);
-    printf("counter 1 is: %d\n",counter1);
-    printf("counter 2 is :%d",counter2);
+    if ((ceil(log2(counter1) != floor(log2(counter1))))){
+        counter1 -=1;
+        counter2++;
+    }
+    else{
+        printf("B is %d",b);
+        printf("counter 1 is: %d",counter1);
+        printf("counter 2 is :%d",counter2);
+    }
 }
+
     return 0;
 }
